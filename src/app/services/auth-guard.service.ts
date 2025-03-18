04c591110a5684fafe56e87d5bf2b7707ce11ff3
Nanthawut Mueangkaeo <nanthawut.mue@pea.co.th>
2025-03-18 16:25:48 +0700

    update ver 15 to 16

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
     authState: any = null;
 
     constructor(private router: Router,
